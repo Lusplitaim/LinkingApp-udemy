906cd9e1dd8f8e40d1a6d3a31f83ebf0269b8007
Pluralment <Foolman.ru@yandex.ru>
2021-09-01 23:14:48 +0300

    ExceptionMiddleware added

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,9 @@ namespace API.DTOs
     {
         [Required]
         public string UserName { get; set; }
+
         [Required]
+        [MinLength(4)]
         public string Password { get; set; }
     }
 }
