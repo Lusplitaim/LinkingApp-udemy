02aed34bbe0bab7fce0e51c51d2cc4bb68bc90ad
Pluralment <Foolman.ru@yandex.ru>
2021-09-07 00:05:30 +0300

    Loader indicator added

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,20 +3,42 @@ import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Member } from '../_models/member';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
-  getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+  getMembers(): Observable<Member[]> {
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
-  getMember(username: string) {
+  getMember(username: string): Observable<Member> {
+    const member = this.members.find(x => x.userName === username);
+    if (member !== undefined) {
+      return of(member);
+    }
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
