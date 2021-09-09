29a9acaf6ca6bb10a229237d56cb7c77aa05bdea
Pluralment <Foolman.ru@yandex.ru>
2021-09-09 20:26:24 +0300

    Photo loading client fix

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,7 +49,10 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            var user = await _context
+                .Users
+                .Include(u => u.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
 
             if (user == null)
                 return Unauthorized("Invalid username");
