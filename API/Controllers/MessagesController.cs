2c5de780044c6443d2fd10e00665b9c24e6b25cc
Pluralment <Foolman.ru@yandex.ru>
2021-09-22 18:04:04 +0300

    Message classes created for backend

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IUserRepository userRepository,
+            IMessageRepository messageRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+            _messageRepository = messageRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(
+            CreateMessageDto createMessageDto)
+        {
+            var userName = User.GetUserName();
+
+            if (userName == createMessageDto.RecipientUsername.ToLower())
+                return BadRequest("Cannot send messaget to yourself");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(userName);
+            var recipient = await _userRepository.GetUserByUsernameAsync(
+                createMessageDto.RecipientUsername);
+
+            if (recipient == null) return NotFound("No recipient found");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+
+            return BadRequest("Failed to send message");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser(
+            [FromQuery] MessageParams messageParams
+        )
+        {
+            messageParams.UserName = User.GetUserName();
+
+            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize,
+                messages.TotalCount, messages.TotalPages);
+
+            return messages;
+        }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUserName();
+            return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
+        }
+    }
+}
