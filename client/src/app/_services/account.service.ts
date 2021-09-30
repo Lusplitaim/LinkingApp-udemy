965a4b4cc24ca056fdbf3de3c6e466afb16e535c
Pluralment <Foolman.ru@yandex.ru>
2021-10-01 00:49:38 +0300

    added admin panel

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -37,13 +37,20 @@ export class AccountService {
     );
   }
 
-  setCurrentUser(user: User){
+  setCurrentUser(user: User): void {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem("user", JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
-  logout() {
+  logout(): void {
     localStorage.removeItem("user");
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
